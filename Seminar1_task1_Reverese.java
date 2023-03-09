eb66528c1bb0404e32f321addef52d20f9606428
NikitaShevnin <mrborman95@gmail.com>
2023-03-09 18:28:05 +0300

    export to github

---
+++ Seminar1_task1_Reverese.java
@@ -0,0 +1,8 @@
+public static String reverseTextString(String str) {
+    var list = str.replaceAll("\\s+", " ").split(" ");
+    String newstr = "";
+    for (int i = 0; i < list.length; i++) {
+        newstr += list[list.length - i - 1] + " ";
+    }
+    return newstr;
+}
