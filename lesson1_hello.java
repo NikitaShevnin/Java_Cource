ea01effc8ffadeac90618558df01fa3a9d56ce90
NikitaShevnin <mrborman95@gmail.com>
2023-03-09 18:16:45 +0300

    example

---
+++ lesson1_hello.java
@@ -0,0 +1,17 @@
+// class Program {
+   
+//     public static void main(String[] args) {
+           
+//         System.out.println("Hello world!");
+//         System.out.println("Bye world...");
+//     }
+// }
+
+class Program 
+{
+    public static void main(String[] args) 
+    {
+    String msg ="Hello world";
+    System.out.println(msg); // Hello world
+    }
+}
