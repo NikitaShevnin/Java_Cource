f2becb489a206d442cba2293f9b72b8550696796
NikitaShevnin <mrborman95@gmail.com>
2023-03-12 13:47:58 +0300

    rename class

--- Seminar1_task1_Reverese.java
+++ Seminar1_task1_Reverese.java
@@ -1,8 +1,13 @@
-public static String reverseTextString(String str) {
-    var list = str.replaceAll("\\s+", " ").split(" ");
-    String newstr = "";
-    for (int i = 0; i < list.length; i++) {
-        newstr += list[list.length - i - 1] + " ";
+public class Seminar1_task1_Reverese
+{
+    public static String reverseTextString(String str) 
+    {
+        var list = str.replaceAll("\\s+", " ").split(" ");
+        String newstr = "";
+        for (int i = 0; i < list.length; i++) 
+        {
+            newstr += list[list.length - i - 1] + " ";
+        }
+        return newstr;
     }
-    return newstr;
 }
