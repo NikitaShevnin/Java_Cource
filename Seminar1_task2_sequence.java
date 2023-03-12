27498cdc9824499f4e538f110ca3334af451fc02
NikitaShevnin <mrborman95@gmail.com>
2023-03-12 14:28:49 +0300

    job done

--- Seminar1_task2_sequence.java
+++ Seminar1_task2_sequence.java
@@ -1,21 +1,26 @@
 //        Дана последовательность целых чисел, оканчивающаяся нулем. 
 //        Найти сумму положительных чисел,
 //        после которых следует отрицательное число.
-//        Пример ввода:
+//        Пример ввода: 1 2 1 2 -1 1 3 1 3 -1 0
 
 import java.util.Scanner;
 
-class program {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int a = sc.nextInt();
-        int b;
-        do {
-            b = sc.nextInt();
-            if (b < 0 & a > 0) sum += a;
-        } while (b != 0);
-        System.out.println("result: " + sum);
-    }
+public class Seminar1_task2_sequence
 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        int a = scan.nextInt();
+        int b;
+        do 
+        {
+            b = scan.nextInt();
+            if (b < 0 & a > 0) sum += a;
+        }
+        while (b != 0);
+        System.out.println("result: " + sum);
+        scan.close();
+    }
 }
